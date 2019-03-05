419aa5e692ab22eecd5750d393febf494c52251d
GinoT <ginod.98@hotmail.com>
2019-03-05 21:23:17 +0100

    added login endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import {getIndexRoute} from './index.route'
 import {postSignupRoute} from './signup.route'
+import {postLoginRoute} from './login.route'
 
 export default [
   getIndexRoute,
-  postSignupRoute
+  postSignupRoute,
+  postLoginRoute
 ]
 
 
